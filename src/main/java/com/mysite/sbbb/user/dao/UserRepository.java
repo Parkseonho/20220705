2042a1876324e35def2763fb52074e07eb12da51
Parkseonho <tjsgh2103@gmail.com>
2022-07-05 12:18:34 +0900

    user추가

---
+++ src/main/java/com/mysite/sbbb/user/dao/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbbb.user.dao;
+
+import com.mysite.sbbb.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
